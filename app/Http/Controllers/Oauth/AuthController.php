43ac6969e14009e3e8db3171491154d8d8f7a475
hnh <1123416584@qq.com>
2018-06-05 15:24:51 +0000

    一点点开发进度，睡觉去了。。。

--- app/Http/Controllers/Oauth/AuthController.php
+++ app/Http/Controllers/Oauth/AuthController.php
@@ -7,8 +7,12 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
+
     public function qq()
     {
-        require_once (app_path('Class/Oauth/Qq/qqConnectAPI.php'));
+        require_once (app_path('Plugs/Class/Oauth/Qq/qqConnectAPI.php'));
+        $qc = new \QC();
+        $qc->qq_login();
     }
+
 }
---
+++ app/Plugs/Class/Oauth/Qq/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101483116","appkey":"fed206aeb8266ad90163dc4fe3536d7f","callback":"https://blog.hnh117.com/api","scope":"get_user_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
